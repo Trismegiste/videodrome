b38484a2457a33eef7e039bd309bf9b92d91e019
Trismegiste <flo@trismegiste.net>
2019-12-27 07:11:04 +0100

    iterating through diapo names

--- impress2yt.php
+++ impress2yt.php
@@ -14,35 +14,27 @@ if (4 !== count($argv)) {
 }
 generateVideo($argv[1], $argv[2], $argv[3]);
 
-//////////
-function getNbPage($fch) {
-    $tmp = shell_exec("pdfinfo " . $fch);
-    $tmp = preg_match('/^Pages:[\s]+([\d]+)$/m', $tmp, $result);
-    $nbPage = (int) $result[1];
-    return $nbPage;
-}
-
 function generateVideo($impress, $marqueur, $voix) {
     $pdfTask = new \videodrome\ImpressToPdf($impress);
     $pdfTask->exec();
     $pdf = $pdfTask->getPdf();
 
     $timecode = file($marqueur);
-    if (count($timecode) !== getNbPage($pdf)) {
-        throw new Exception("page count mismatch");
-    }
 
     $diapoTask = new \videodrome\PdfToPng($pdf);
+    if (count($timecode) !== $diapoTask->getPdfPageCount()) {
+        throw new Exception("page count mismatch");
+    }
     $diapoTask->exec();
 
     $vidname = [];
     $vidGen = new \videodrome\LoopTask();
-    foreach ($timecode as $idx => $diapo) {
-        $detail = preg_split('/[\s]+/', $diapo);
+    foreach ($diapoTask->getDiapoName() as $idx => $diapo) {
+        $detail = preg_split('/[\s]+/', $timecode[$idx]);
         $delta = $detail[1] - $detail[0];
         $output = "vid-$idx.avi";
         $vidname[] = $output;
-        $obj = new videodrome\PngToVideo("diapo-$idx.png", $delta, $output);
+        $obj = new videodrome\PngToVideo($diapo, $delta, $output);
         $vidGen->push($obj);
     }
     $vidGen->exec();
--- lib/videodrome/PdfToPng.php
+++ lib/videodrome/PdfToPng.php
@@ -8,6 +8,7 @@ namespace videodrome;
 class PdfToPng implements Task {
 
     private $pdf;
+    private $pageCount = null;
 
     public function __construct($fch) {
         $this->pdf = $fch;
@@ -19,6 +20,25 @@ class PdfToPng implements Task {
 
     public function exec() {
         shell_exec("convert -density 200 {$this->pdf} -resize 1920x1080 diapo.png");
+    }
+
+    public function getPdfPageCount() {
+        if (is_null($this->pageCount)) {
+            $tmp = shell_exec("pdfinfo " . $this->pdf);
+            $tmp = preg_match('/^Pages:[\s]+([\d]+)$/m', $tmp, $result);
+            $this->pageCount = (int) $result[1];
+        }
+
+        return $this->pageCount;
+    }
+
+    public function getDiapoName() {
+        $r = [];
+        for ($k = 0; $k < $this->getPdfPageCount(); $k++) {
+            $r[] = "diapo-$k.png";
+        }
+
+        return $r;
     }
 
 }
