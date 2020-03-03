2e47d43db9bcff97e44412654962180b458f0edc
Trismegiste <flo@trismegiste.net>
2020-03-03 08:44:48 +0100

    video concat ok

---
+++ src/Chain/Job/VideoConcat.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Trismegiste\Videodrome\Chain\Job;
+
+use Symfony\Component\Process\Process;
+use Trismegiste\Videodrome\Chain\FileJob;
+use Trismegiste\Videodrome\Chain\JobException;
+
+/**
+ * Concat a list of video into one mp4
+ */
+class VideoConcat extends FileJob {
+
+    protected function process(array $filename, array $unused): array {
+        if (count($filename) <= 1) {
+            throw new JobException("VideoConcat : Not enough video to concat");
+        }
+        $output = pathinfo($filename[0], PATHINFO_FILENAME) . '-compil.mp4';
+
+        $ffmpeg = new Process('ffmpeg -y -i "concat:' . implode('|', $filename) . '" ' . $output);
+        $ffmpeg->setTimeout(null);
+        $ffmpeg->run();
+
+        if (!$ffmpeg->isSuccessful()) {
+            throw new JobException('VideoConcat : Fail to concat ' . implode('|', $filename));
+        }
+        if (!file_exists($output)) {
+            throw new JobException("VideoConcat : $output does not exist");
+        }
+
+        return [$output];
+    }
+
+}
---
+++ tests/Chain/Job/VideoConcatTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Trismegiste\Videodrome\Chain\Job\ImpressToPdf;
+use Trismegiste\Videodrome\Chain\Job\PdfToPng;
+use Trismegiste\Videodrome\Chain\Job\PngToVideo;
+use Trismegiste\Videodrome\Chain\Job\VideoConcat;
+
+class VideoConcatTest extends TestCase {
+
+    public function testExecute() {
+        $sut = new VideoConcat(new PngToVideo(new PdfToPng(new ImpressToPdf())));
+        $ret = $sut->execute([__DIR__ . '/fixtures1.odp'], ['duration' => [1, 1, 1]]);
+        $this->assertEquals(['fixtures1-0-compil.mp4'], $ret);
+        $this->assertTrue(file_exists($ret[0]));
+        // clean
+        //   unlink($ret[0]);
+    }
+
+}
