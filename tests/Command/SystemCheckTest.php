70fc2c51138b7104c7bbe41ac65c3d5e1af4a952
Trismegiste <flo@trismegiste.net>
2020-03-04 16:16:25 +0100

    test for system check cmd

---
+++ tests/Command/SystemCheckTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Trismegiste\Videodrome\Command\SystemCheck;
+
+class SystemCheckTest extends TestCase {
+
+    public function testExecute() {
+        $application = new Application();
+        $application->setAutoExit(false);
+        $command = new SystemCheck();
+        $application->add($command);
+
+        $tester = new CommandTester($application->find('system:check'));
+
+        $this->assertEquals(0, $tester->execute([]));
+        $this->assertRegExp("/alright/", $tester->getDisplay());
+    }
+
+}
