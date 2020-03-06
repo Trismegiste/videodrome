f6f86e1323f89d211c605a6cc551f28ada374698
Trismegiste <flo@trismegiste.net>
2020-03-06 14:48:09 +0100

    more usable system check cmd

--- src/Command/SystemCheck.php
+++ src/Command/SystemCheck.php
@@ -3,7 +3,6 @@
 namespace Trismegiste\Videodrome\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
@@ -28,18 +27,14 @@ class SystemCheck extends Command {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
+        $output->writeln(["Checking installed softwares", '============================', '']);
         // check installed dependencies
         foreach (self::dependencies as $app) {
-            $output->write("Checking for {$app[2]}...");
+            $output->write("Checking for {$app[2]}... ");
             $check = new Process([$app[0], $app[1]]);
             $check->run();
-            if (!$check->isSuccessful()) {
-                throw new RuntimeException($app[2] . ' is missing');
-            }
-            $output->writeln(" OK");
+            $output->writeln($check->isSuccessful() ? "OK" : "FAIL");
         }
-
-        $output->writeln("Everything seems alright");
     }
 
 }
