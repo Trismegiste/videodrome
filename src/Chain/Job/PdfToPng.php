4f393c82c8e26960af606728cb8b03c69a844e8a
Trismegiste <flo@trismegiste.net>
2020-03-07 05:33:09 +0100

    need more thinking

--- src/Chain/Job/PdfToPng.php
+++ src/Chain/Job/PdfToPng.php
@@ -6,6 +6,7 @@ use RuntimeException;
 use Symfony\Component\Process\Process;
 use Trismegiste\Videodrome\Chain\FileJob;
 use Trismegiste\Videodrome\Chain\JobException;
+use Trismegiste\Videodrome\Chain\MediaList;
 use Trismegiste\Videodrome\Chain\MetaFileInfo;
 use Trismegiste\Videodrome\Util\PdfInfo;
 
@@ -16,7 +17,7 @@ class PdfToPng extends FileJob {
 
     const antialiasFactor = 1.3;
 
-    protected function process(array $filename): array {
+    protected function process(MediaList $filename): MediaList {
         list($pdf) = $filename;
         $info = new PdfInfo($pdf);
         $dpi = $info->getMinDensityFor($pdf->getData('width'), $pdf->getData('height')) * self::antialiasFactor;
@@ -38,7 +39,9 @@ class PdfToPng extends FileJob {
             for ($k = 0; $k < $card; $k++) {
                 $tmpname = $exportName . "-$k.png";
                 // manage metadata
-                $metadata = $pdf->getMetadata();
+                $metadata = $pdf->getMetadata(); FAIL => Où placer les metadata de durée ? Dans le MetaFileInfo ou le MediaList ?
+                EN fait, les classes partagent des points communs et on peut se demander s'ils'agit d'un Tree ?
+                    Les createChild sont liés et l'héritage des metadata aussi
                 // explode duration metadata for each PNG
                 if (array_key_exists('duration', $metadata)) {
                     $metadata['duration'] = $metadata['duration'][$k];
