4c1f8808c5710dcb5f040ab10b74f80e14b9d67f
Trismegiste <flo@trismegiste.net>
2020-03-07 04:32:36 +0100

    media list feature

---
+++ src/Chain/MediaList.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Trismegiste\Videodrome\Chain;
+
+/**
+ * A group of MetaFileInfo with metadata
+ */
+class MediaList implements \ArrayAccess, \Countable, \IteratorAggregate {
+
+    protected $list;
+    protected $metadata;
+
+    public function __construct(array $group = [], array $metadata = []) {
+        foreach ($group as $key => $fch) {
+            if (!($fch instanceof MetaFileInfo)) {
+                throw new \UnexpectedValueException("File with key '$key' is not a MetaFileInfo");
+            }
+            $this->list[] = $fch;
+        }
+        $this->metadata = $metadata;
+    }
+
+    public function count(): int {
+        return count($this->list);
+    }
+
+    public function offsetExists($offset): bool {
+        return array_key_exists($offset, $this->list);
+    }
+
+    public function offsetGet($offset) {
+        return $this->list[$offset];
+    }
+
+    public function offsetSet($offset, $value) {
+        if (!($value instanceof MetaFileInfo)) {
+            throw new \UnexpectedValueException("File with key '$offset' is not a MetaFileInfo");
+        }
+        $this->list[$offset] = $value;
+    }
+
+    public function offsetUnset($offset) {
+        unset($this->list[$offset]);
+    }
+
+    public function getIterator(): \Traversable {
+        return new \ArrayIterator($this->list);
+    }
+
+    public function getData(string $key) {
+        return $this->metadata[$key];
+    }
+
+    public function createChild(array $group, array $override): MediaList {
+        return new MediaList($group, array_merge($this->metadata, $override));
+    }
+
+}
---
+++ tests/Chain/MediaListTest.php
@@ -0,0 +1,73 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Trismegiste\Videodrome\Chain\MediaList;
+use Trismegiste\Videodrome\Chain\MetaFileInfo;
+
+class MediaListTest extends TestCase {
+
+    public function simpleFixture() {
+        return [[new MetaFileInfo(__FILE__)]];
+    }
+
+    public function testEmptyCreation() {
+        $sut = new MediaList();
+        $this->assertCount(0, $sut);
+    }
+
+    /** @dataProvider simpleFixture */
+    public function testPushing($file) {
+        $sut = new MediaList();
+        $sut[] = $file;
+        $this->assertCount(1, $sut);
+    }
+
+    /** @dataProvider simpleFixture */
+    public function testCreationWithArray($file) {
+        $sut = new MediaList([$file]);
+        $this->assertCount(1, $sut);
+    }
+
+    /** @dataProvider simpleFixture */
+    public function testGetter($file) {
+        $sut = new MediaList([$file]);
+        $this->assertEquals($file, $sut[0]);
+    }
+
+    /** @dataProvider simpleFixture */
+    public function testIterator($file) {
+        $sut = new MediaList([$file, $file, $file]);
+        foreach ($sut as $item) {
+            $this->assertEquals($file, $item);
+        }
+    }
+
+    /** @dataProvider simpleFixture */
+    public function testUnsetKey($file) {
+        $sut = new MediaList([$file, $file, $file]);
+        $this->assertCount(3, $sut);
+        unset($sut[1]);
+        $this->assertCount(2, $sut);
+        $this->assertArrayNotHasKey(1, $sut);
+    }
+
+    /** @dataProvider simpleFixture */
+    public function testMetadataGetter($file) {
+        $sut = new MediaList([$file], ['width' => 1920]);
+        $this->assertEquals(1920, $sut->getData('width'));
+    }
+
+    /** @dataProvider simpleFixture */
+    public function testCreateChild($file) {
+        $sut = new MediaList([$file], ['height' => 1080, 'width' => 1920]);
+        $child = $sut->createChild([$file], ['width' => 2500]);
+        $this->assertEquals(2500, $child->getData('width'));
+        $this->assertEquals(1080, $child->getData('height'));
+    }
+
+    /** @expectedException \UnexpectedValueException */
+    public function testCheckType() {
+        new MediaList([new stdClass()]);
+    }
+
+}
