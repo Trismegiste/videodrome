67aa39746f0aaaed30b08c8a825e943024239f2f
Trismegiste <flo@trismegiste.net>
2020-03-12 22:55:46 +0100

    func test concat cmd ok

--- tests/Command/EditingConcatYtTest.php
+++ tests/Command/EditingConcatYtTest.php
@@ -1,6 +1,33 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Trismegiste\Videodrome\Command\EditingConcatYt;
+use Trismegiste\Videodrome\Command\EditingConfig;
+use Trismegiste\Videodrome\Util\Ffprobe;
 
-class EditingConcatYtTest {
-    //put your code here
+class EditingConcatYtTest extends TestCase {
+
+    public function testExecute() {
+        $this->assertFileNotExists(EditingConfig::defaultCfgName);
+        $application = new Application();
+        $application->setAutoExit(false);
+        $command = new EditingConcatYt();
+        $application->add($command);
+        $tester = new CommandTester($application->find('edit:youtube'));
+
+        $output = 'output.mp4';
+        $this->assertEquals(0, $tester->execute([
+                    'config' => join_paths(__DIR__, '../fixtures', 'editconcat.json'),
+                    '--width' => 300,
+                    '--height' => 200
+        ]));
+
+        $this->assertFileExists($output);
+        $info = new Ffprobe($output);
+        $this->assertLessThan(0.1, abs($info->getDuration() - 6));  // rounding frame
+        unlink($output);
+    }
+
 }
